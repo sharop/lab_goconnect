91ea0d85a6ccfcfcb48f0f0fec362abf4e85feaf
sharop <sharop@gmail.com>
2021-11-22 12:20:21 -0600

    Client for servers

--- cmd/getservers/main.go
+++ cmd/getservers/main.go
@@ -1,1 +1,29 @@
-package getservers
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	api "github.com/sharop/lab_goconnect/api/v1"
+	"google.golang.org/grpc"
+	"log"
+)
+
+func main(){
+	addr := flag.String("addr", ":10500", "service address")
+	flag.Parse()
+	conn, err := grpc.Dial(*addr, grpc.WithInsecure())
+	if err != nil{
+		log.Fatalln(err)
+	}
+	client := api.NewLogClient(conn)
+	ctx := context.Background()
+	res, err := client.GetServers(ctx, &api.GetServersRequest{})
+	if err != nil{
+		log.Fatal(err)
+	}
+	fmt.Println("servers:")
+	for _, server := range res.Servers{
+		fmt.Printf("\t- %v\n", server)
+	}
+}
